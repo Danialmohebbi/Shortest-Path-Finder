62e00190dc085eb83e03acd70e2e19b60556dde5
Danialmohebbi <daniyalmohebbi@yahoo.com>
2024-07-20 13:10:32 +0200

    Create Graph

--- A_Star.py
+++ A_Star.py
@@ -1,5 +1,7 @@
+import json
 import math
-
+import pandas as pd
+import networkx as nx
 class PriorityQueue:
     def __init__(self):
         self.data = []
@@ -70,12 +72,13 @@ class PriorityQueue:
 
 
 class Edge:
-    def __init__(self, source, sink, weight, direction, next):
+    def __init__(self, source, sink, weight,departure_time, direction, next):
         self.source = source
         self.sink = sink
         self.weight = weight
         self.direction = direction
         self.next = next
+        self.departure_time = departure_time
 
 
 class Graph:
@@ -91,13 +94,13 @@ class Graph:
         if self.m.get(v) is None:
             self.m[v] = None
 
-    def addEdge(self, source, sink, weight, direction):
+    def addEdge(self, source, sink, weight,departure_time, direction):
         if weight < 0:
             raise ValueError("Graph contains negative edge weight, which is not allowed for A* algorithm.")
         if self.m.get(source) is None:
-            self.m[source] = Edge(source, sink, weight, direction, None)
+            self.m[source] = Edge(source, sink, weight,departure_time, direction, None)
         else:
-            Node = Edge(source, sink, weight, direction, self.m[source])
+            Node = Edge(source, sink, weight,departure_time, direction, self.m[source])
             self.m[source] = Node
 
     def getNeighbours(self, source):
@@ -157,7 +160,7 @@ def A_Star(graph, source, goal):
         while edge is not None:
             w = edge.sink
             if state[w] != "closed":
-                tenative_g = g[x] + edge.weight
+                tenative_g = g[x] + edge.weight + edge.departure_time
                 if  tenative_g < g[w]:
                     g[w] = tenative_g
                     f[w] = tenative_g + h(graph.vertices_cords[w], graph.vertices_cords[goal])
@@ -171,27 +174,51 @@ def A_Star(graph, source, goal):
         path.insert(0, goal)
         goal = previous[goal]
 
-    print(state)
+    with open('magic.txt', 'w') as f:
+        for key, value in state.items():
+            f.write(f"{key}:{value}\n")
     return path
 
-
+stops = pd.read_csv("pid_gtfs/stops.txt")
+stop_times = pd.read_csv('pid_gtfs/stop_times.txt', low_memory=False)
+trips = pd.read_csv('pid_gtfs/trips.txt', low_memory=False)
+routes = pd.read_csv('pid_gtfs/routes.txt', low_memory=False)
 g = Graph()
-g.addVertice("A", 1, 0)
-g.addVertice("B", 2, 0)
-g.addVertice("C", 3, 0)
-g.addVertice("D", 2, 1)
-g.addVertice("E", 4, 0)
-g.addVertice("G", 0, 1)
-g.addVertice("H", 0, 2)
-g.addVertice("F", 0, 0)
+station = {}
+for index, row in stops.iterrows():
+    g.addVertice(row['stop_id'],row['stop_lat'], row['stop_lat'])
+    station[row['stop_id']] = row['stop_name']
 
-g.addEdge("A", "B", 7, True)
-g.addEdge("A", "E", 1, True)
-g.addEdge("B", "E", 8, True)
-g.addEdge("B", "C", 3, True)
-g.addEdge("E", "C", 2, True)
-g.addEdge("E", "D", 7, True)
-g.addEdge("C", "D", 6, True)
+def timeToSecond(current_time):
+    list_of_time = current_time.split(':')
+    print(list_of_time)
+    time = 0
+    for i in range(len(list_of_time)):
+            num = int(list_of_time[i])
+            time += num * 60 ** (len(list_of_time) - i - 1)
+    return time
 
-path = A_Star(g, "A", "D")
+def convert_to_time(x):
+    km_h = 50
+    return (x / km_h) * 60 
+
+for index, row in stop_times.iterrows():
+    
+    trip_id = row['trip_id']
+    stop_id = row['stop_id']
+    
+    next_index = index + 1 # type: ignore
+
+    if next_index < len(stop_times) and stop_times.iloc[next_index]['trip_id'] == trip_id:
+        next_stop_id = stop_times.iloc[next_index]['stop_id']
+        departure_time = timeToSecond(stop_times.iloc[index]['departure_time'])
+        arrival_time = timeToSecond(stop_times.iloc[next_index]['arrival_time'])
+        
+        time = (arrival_time - departure_time) / 60.0
+        
+        g.addEdge(stop_id, next_stop_id, time, 0, True)
+
+path = A_Star(g, "U536Z3P", "U361Z1P")
+for i in range(len(path)):
+    path[i] = station[path[i]]  
 print("Path:", path)
